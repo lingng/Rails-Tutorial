6e33ccea5f1fa57632c89e8931b713b1e752c7c1
YutingZhang <lizlingng@gmail.com>
2015-06-05 16:18:43 +0900

    Modify basic css and html.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
  
   def index
     @articles = Article.all
